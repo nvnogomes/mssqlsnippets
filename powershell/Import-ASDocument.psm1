22e82a2466977b6d61e81d819c2b00772f9f9916
Nuno Gomes <nuno.gomes@unipartner.com>
2020-05-06 19:03:48 +0100

    Added new powershell directory; New snippet for merge statements;

---
+++ powershell/Import-ASDocument.psm1
@@ -0,0 +1,64 @@
+﻿<#
+.Synopsis
+   This module is used to import csv data to the database
+.DESCRIPTION
+   Initial phase 
+.EXAMPLE
+   Import-ASDocument -ConnectionString "Data Source=$Server; Integrated Security=True;" -ASDocument "CRVFX205_Entidade" -Database "AJW" -Directory "C:\"
+#>
+function Import-ASDocument {
+
+    [CmdletBinding()]
+    Param (
+        # ConnectionString Connectionstring to the destination instance
+        [Parameter(Mandatory=$true)]
+        [string] $ConnectionString,
+
+        # ASDocument Name of the document and table to be used
+        [Parameter(Mandatory=$true)]
+        [ValidateSet("CRVFX212_MbRef", "CRVFX203_Operacao","CRVFX204_Contrato", `
+            "CRVFX205_Entidade", "CRVFX206_Interveniente", "CRVFX207_Advogado",  `
+            "CRVFX208_Recuperador", "CRVFX209_Solicitador", "CRVFX210_Balcao",  `
+            "CRVFX211_TipoInterveniente", "CRVFX219_ÁreasGeografica",  ` 
+            "CRVFX220_DocumentoIdentificacao", "CRVFX221_DetalheDivida", "CRVFX222_Acordo",  `
+            "CRVFX223_AcordoPrestacao","CRVFX224_DividaAcordo","CRVFX225_Colateral",  `
+            "CRVFX226_FamiliaColateral", "CRVFX227_TipoColateral", "CRVFX228_TipoDocumentoIdentificacao",  `
+            "CRVFX229_EstadoProcessual","CRVFX230_EstadoDivida","CRVFX231_EstadoAcordo", "CRVFX213_OperacoesCRE")]
+        [string] $ASDocument,
+
+        [Parameter(Mandatory=$true)]
+        [string] $Database,
+
+        [Parameter(Mandatory=$true)]
+        [string] $Directory
+    )
+    Process {
+
+        $crvFile =  $ASDocument.Split("_")[0] + "*.dat"
+        # remove quotes
+        cd $Directory
+        $lastCrvfxFile = Get-ChildItem $crvFile | Sort-Object CreationTime -Descending | Select-Object -First 1
+        (Get-Content $lastCrvfxFile) -replace '"', '' | Out-File -FilePath $ASDocument -Force -Encoding ascii
+
+
+         $importCsvParams = @{
+            # fileinfo
+            Path = $ASDocument
+            Encoding = "ASCII"
+            Delimiter = ";"
+            NoHeaderRow = $true
+            TrimmingOption = "All"
+
+            # database info
+            SqlInstance = $ConnectionString
+            Database = $Database
+            Schema = "import"
+            Table = $ASDocument
+            Truncate = $true
+            KeepOrdinalOrder = $true
+            KeepNulls = $true
+        }
+        Import-DbaCsv @importCsvParams
+    }
+}
+#Export-ModuleMember -Function 'Import-ASDocument'
---
+++ powershell/New-Archive.psm1
@@ -0,0 +1,56 @@
+﻿<#
+.Synopsis
+   File archiver
+.DESCRIPTION
+   Archives all files that match the given regex. 
+   The files archived will be deleted from the original location.
+.EXAMPLE
+   New-Archive -BaseDirectory "C:\Pdfs" -FileRegex "*.pdf" -ArchivePrefix "PdfArchive"
+#>
+function New-Archive {
+    [CmdletBinding()]
+    Param (
+        # BaseDirectory Directory to be the used as base to the commands
+        [Parameter(Mandatory=$true)]
+        [string]
+        $BaseDirectory,
+
+        # FileRegex Files in the base directory to be archived
+        [Parameter(Mandatory=$true)]
+        [string]
+        $FileRegex,
+
+
+        # ArchivePrefix Prefix to the archive to be created
+        [Parameter(Mandatory=$true)]
+        [string]
+        $ArchivePrefix 
+    )
+    Begin {
+        # setup variables
+        $RegexExp = Join.Path -Path $directory -ChildPath $FileRegex
+        $archiveDirectory = Join-Path -Path $directory -childPath "archive"
+        $archiveFilename =  $ArchivePrefix + (Get-Date -Format "yyyyMMdd") + ".zip"
+
+        md $archiveDirectory -Force | Out-Null
+    }
+
+    Process {
+
+        # create archive
+        $compress = @{
+            Path= $RegexExp
+            CompressionLevel = "Fastest"
+            DestinationPath = Join-Path -Path $archiveDirectory -ChildPath $archiveFilename
+            Force = $true
+        }
+        Compress-Archive @compress
+
+    }
+
+    End {
+        # delete archived documents
+        Remove-Item -Path $RegexExp
+    }
+}
+Export-ModuleMember -Function New-Archive
